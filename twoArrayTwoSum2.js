7ba3b16bc67fb746072a7257d55a92ab9b5457ce
Buck <buckm@alumni.stanford.edu>
2026-07-08 09:59:22 -0700

    Describe approach to solution

--- twoArrayTwoSum2.js
+++ twoArrayTwoSum2.js
@@ -27,3 +27,9 @@
 // - `sorted_arr` is sorted in ascending order
 // - The arrays have no duplicates
 // - Use `O(1)` _extra space_ and do not modify the input
+
+/*
+Approach:
+For each item in the unsorted array, do a binary search on its 0-complement in the sorted array.
+*/
+
